cc1e0b4863486752adbcc52b4fda5d6aef13b178
Sanjay <sanjaymunda0123@gmail.com>
2023-04-22 14:02:06 +0530

    added header, footer, css, js and fontawesome files from old osm project

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,6 @@
+@extends('frontend.layouts.main')
+@push('title')
+    <title>Online Shopping Mart</title>
+@endpush
+@section('main-section')
+@endsection
---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,60 @@
+<!--FOOTER STARTS-->
+<footer>
+    <div class="footer">
+        <div class="group">
+            <h4>ONLINE SHOPPING</h4>
+            <ul>
+                <li><a href="#">Men</a></li>
+                <li><a href="#">Women</a></li>
+                <li><a href="#">Kids</a></li>
+                <li><a href="#">Home & Living</a></li>
+                <li><a href="#">Beauty</a></li>
+                <li><a href="#">Gifts Cards<span>New</span></a></li>
+            </ul>
+        </div>
+        <div class="group">
+            <h4>USEFUL LINKS</h4>
+            <ul>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">T&C</a></li>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Track Orders</a></li>
+                <li><a href="#">Shipping</a></li>
+                <li><a href="#">Cancellation</a></li>
+                <li><a href="#">Returns</a></li>
+                <li><a href="#">Whitehat</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Site Map</a></li>
+            </ul>
+        </div>
+        <div class="group extend25">
+            <h4>EXPERIENCE OUR APP ON MOBILE</h4>
+            <div class="appstore"><img src="{{ url('assets/images/google_play.png') }}" alt="app"><img
+                    src="{{ url('assets/images/apple_store.png') }}" alt="app"></div>
+            <h4>KEEP IN TOUCH</h4>
+            <div class="social-icons">
+                <i class="fab fa-facebook-square"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-youtube"></i>
+                <i class="fab fa-instagram"></i>
+            </div>
+        </div>
+        <div class="group extend35">
+            <div class="grantee">
+                <img src="{{ url('assets/images/original.png') }}" alt="100%">
+                <p><strong>100% ORIGINAL </strong>guarantee for all products at Online Shpping Mart</p>
+            </div>
+            <div class="return30"><img src="{{ url('assets/images/30days.png') }}" alt="return30">
+                <p><strong>Return wwithin 30days </strong>of receiving your order</p>
+            </div>
+        </div>
+    </div>
+    <div class="creater">
+        <p>CREATED BY Sanjay Munda</p>
+    </div>
+</footer>
+<!--FOOTER ENDS-->
+<script src="{{ url('assets/js/script.js') }}"></script>
